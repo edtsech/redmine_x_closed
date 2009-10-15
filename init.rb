a586b75d29ce2ecd712fb18bfe719f9c2db832d8
ed <edtsech@gmail.com>
2009-10-15 11:54:31 +0700

    blank plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_issue_closed do
+  name 'Redmine Issue Closed plugin'
+  author 'Equelli'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
