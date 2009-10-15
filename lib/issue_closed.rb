1a4e56d1b24a3c47fb08d7a6f5d7b75d4faea055
ed <edtsech@gmail.com>
2009-10-15 15:12:05 +0700

    fix issue_closed & add IssueResolved method to Issue model

--- lib/issue_closed.rb
+++ lib/issue_closed.rb
@@ -1,11 +1,21 @@
 module IssueClosed
- module IssueStatus
-   def self.first_closed
-     find(:first, :conditions =>["is_closed=?", true], :order => 'position').id
-   end
- end
+  module IssueStatus
+    def self.first_closed
+      find(:first, :conditions =>["is_closed=?", true], :order => 'position').id
+    end
+  end
+ 
+  module IssueResolved
+    def resolved?
+      self.status.is_resolved?
+    end
+  end
 end
 
 class IssueStatus
   include IssueClosed::IssueStatus
+end
+
+class Issue
+  include IssueResolved::Issue
 end
