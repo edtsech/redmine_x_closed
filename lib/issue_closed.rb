a8e63550d0941d6c6ab20c87b0438d6c756a39c0
ed <edtsech@gmail.com>
2009-10-18 03:14:32 +0700

    created status change handler

--- lib/issue_closed.rb
+++ lib/issue_closed.rb
@@ -10,27 +10,63 @@ module IssueClosed
   #    self.status.is_resolved?
   #  end
   #end
-  
+      
   module IssueStatusesController
-    module InstanceMethods
-    end
-    
     def self.included base
-      base.send :include, InstanceMethods
       base.class_eval do
-        alias _list list
         
+        alias _list list
+
         def index
           list
-          render :template => 'issue_statuses/issue_closed_list' #'list' unless request.xhr?
         end
         
         def list
-          _list
-          render :template => 'issue_statuses/issue_closed_list' #'list' unless request.xhr?
+          collect_issue_statuses
+          render :template => 'issue_statuses/issue_closed_list'
+        end
+        
+        def update_issue_closed
+          (statuses = IssueStatus.all).each do |status|
+            case status.id
+            when params[:closed].to_i
+              status.state = true
+            when params[:resolved].to_i
+              status.state = false
+            else
+              status.state = nil
+            end
+            status.save!
+          end
+          redirect_to :action => :list
+        end
+      protected
+        def collect_issue_statuses
+          @issue_status_pages, @issue_statuses = paginate :issue_statuses, :per_page => 25, :order => "position"
         end
       end
    end
+  end
+
+  module IssuesController 
+    def self.included base
+      base.class_eval do
+        alias _edit edit
+        
+        def edit
+          status_before_update = @issue.status
+          _edit
+          if request.post? and \
+            not (@issue.project.enabled_modules.detect { |enabled_module| enabled_module.name == 
+              'issue_closed' }) == nil and \
+            status_before_update != @issue.status and \
+            @issue.status.state == false
+            
+            ###
+          end
+        end
+      end
+    end    
   end
 end
 
@@ -60,4 +96,5 @@ require 'dispatcher'
     end
 
   IssueStatusesController.send :include, IssueClosed::IssueStatusesController
+  IssuesController.send :include, IssueClosed::IssuesController
 end
