740b9403fd9ea57a05ddeb97f92192d77a257e9a
Edward Tsech <edtsech@gmail.com>
2009-10-21 15:35:20 +0700

    clean init.rb

--- init.rb
+++ init.rb
@@ -8,10 +8,7 @@ Redmine::Plugin.register :redmine_issue_closed do
   author 'Equelli'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  #permission :issue_closed, { :example => :say_hello }
-  
-  menu :project_menu, :issue_closed, { :controller => 'example', :action => 'say_hello' }, :caption => 'Sample'
-  
+
   project_module "issue_closed" do
     permission :run_autoclose, { :issue => :update }
   end
